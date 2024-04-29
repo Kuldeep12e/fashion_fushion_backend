d67941de374b7d0b49a578edf684a6f624164265
Kuldeep Yadav <109398194+Kuldeep12e@users.noreply.github.com>
2024-04-29 11:02:31 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,16 +4,21 @@ const ErrorHandler = require('../utils/errorHandler');
 const asyncErrorHandler = require('./asyncErrorHandler');
 
 exports.isAuthenticatedUser = asyncErrorHandler(async (req, res, next) => {
-
     const { token } = req.cookies;
 
     if (!token) {
-        return next(new ErrorHandler("Please Login to Access", 401))
+        return next(new ErrorHandler("Please Login to Access", 401));
     }
-     console.log(req.user.token);
 
     const decodedData = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decodedData.id);
+
+    if (!req.user) {
+        return next(new ErrorHandler("User not found", 404));
+    }
+
+    console.log(req.user.token); // Now accessing req.user.token after it's assigned
+
     next();
 });
 
